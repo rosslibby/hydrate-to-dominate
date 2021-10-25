dd7ff04e35ffeb15969b38442f39df7092027f49
Ross Libby <rosslibby92@gmail.com>
2021-10-25 16:08:11 -0700

    Update dominate.ps1

--- dominate.ps1
+++ dominate.ps1
@@ -1,17 +1,6 @@
-Install-Module -Name BurntToast
-
 [void][Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
 
 Add-Type -AssemblyName System.Windows.Forms
-Add-Type -Name ConsoleUtils -Namespace WPIA -MemberDefinition @'
-  [DllImport("Kernal32.dll")]
-  public static extern IntPtr GetConsoleWindow();
-  [DllImport("user32.dll")]
-  public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
-'@
-
-$hWnd = [WPIA.ConsoleUtils]::GetConsoleWindow()
-[WPIA.ConsoleUtils]::ShowWindow($hWnd, 0)
 
 params($sleep = 1790)
 $WShell = New-Object -com "Wscript.Shell"
@@ -30,7 +19,12 @@ while ($true)
   $WShell.sendKeys("{SCROLLLOCK}")
   Start-Sleep -Milliseconds 200
   $WShell.sendKeys("{SCROLLLOCK}")
-  New-BurntToastNotification -Text "Hydration Alert", "It's time to hydrate. Try to drink at least 8 ounces of water! #HydrateToDominate" -AppLogo C:\hydrate\logo.png
+
+  $wshell = New-Object -ComObject Wscript.Shell
+  $Output = $wshell.Popup("Remember to drink at least 8 ounces of water!")
+  Sleep 1
+  $wshell.SendKeys('~')
+  
   $p = [System.Windows.Forms.Cursor]::Position
   $x = $p.X + $ud
   $y = $p.Y + $ud
